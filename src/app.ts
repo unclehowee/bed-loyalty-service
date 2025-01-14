f8ef938006a11a5f8c17045f3377aaeda85a649c
rrc_F2024WI_djanaban <151777203+unclehowee@users.noreply.github.com>
2025-01-14 14:46:15 -0600

    Revert "feat: added platinum status for users with 1000 points or more, also …"

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,7 @@ import express, { Request, Response, Express } from "express";
 interface Customer {
 	id: number;
 	name: string;
-	status: "GOLD" | "SILVER" | "BRONZE" | "PLATINUM";
+	status: "GOLD" | "SILVER" | "BRONZE";
 	points: number;
 	lastPurchaseDate: string;
 	email?: string;
@@ -82,15 +82,9 @@ app.post("/api/customers/:id/purchase", (req: Request, res: Response): void => {
 	customer.points += Math.floor(purchaseAmount / 10);
 	customer.lastPurchaseDate = new Date().toISOString();
 
-    if(customer.points >= 1000){
-        customer.status = "PLATINUM"
-        customer.lastStatusChange = new Date().toISOString();
-    }
-	else if (customer.points >= 750) {
+	if (customer.points >= 750) {
 		customer.status = "GOLD";
 		customer.lastStatusChange = new Date().toISOString();
-        customer.points += Math.floor((purchaseAmount / 10) + (purchaseAmount * .20))
-
 	} else if (customer.points >= 500) {
 		customer.status = "SILVER";
 		customer.lastStatusChange = new Date().toISOString();
